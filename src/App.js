567bc48e4740e94444d4442806e6a7425606d56a
Gavin56 <gavinobrien56@gmail.com>
2021-01-31 15:49:07 -0500

    Particles working on all pages.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from "react";
 // import projects from "./utils/JSON";
-// import Particles from "react-tsparticles";
+import Particle from "./components/Particles";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -10,10 +10,31 @@ import Portfolio from "./pages/Portfolio";
 function App() {
   return (
     <Router>
-      <div>
-        <Route exact path="/" component={About}/>
-        <Route exact path="/contact" component={Contact}/>
-        <Route exact path="/portfolio" component={Portfolio}/>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Particle />
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+          }}
+        >
+
+          {/* You can render <Route> and <NavTabs /> here */}
+          <Route exact path="/" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        </div>
       </div>
     </Router>
   );
